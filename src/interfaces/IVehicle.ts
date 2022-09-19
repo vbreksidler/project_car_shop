5116aec2e8cd8d37bff4c84fb5f7d74c789bafd3
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-09-19 09:41:16 -0300

    fix: lint

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -5,19 +5,19 @@ const IVehicleZodSchema = z.object({
     required_error: 'Model is required',
     invalid_type_error: 'Model must be a string',
   })
-  .min(3, { 
-    message: 'Model must be 3 or more characters long', 
-  }),
+    .min(3, { 
+      message: 'Model must be 3 or more characters long', 
+    }),
   year: z.number({
     required_error: 'Year is required',
     invalid_type_error: 'Year must be a number',
   })
-  .gte(1900, { 
-    message: 'Year must be 1900 or greater', 
-  })
-  .lte(2022, { 
-    message: 'Year must be 2022 or lesser', 
-  }),
+    .gte(1900, { 
+      message: 'Year must be 1900 or greater', 
+    })
+    .lte(2022, { 
+      message: 'Year must be 2022 or lesser', 
+    }),
   color: z
     .string({
       required_error: 'Color is required',
