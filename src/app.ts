edf265a817a067d6f018cdb36a6c0f0b63661f11
Victor Bariatto Reksidler <vbreksidler@gmail.com>
2022-09-19 12:22:04 -0300

    feat: CRUD Car

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carsRouter from './routes/Cars.route';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carsRouter);
+
+app.use(errorHandler);
 
 export default app;
